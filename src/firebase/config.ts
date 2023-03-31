37ddbc5b08f50ef8b83b40974f101f47b3593f26
Brandon Kelly <63052225+brand-kelly@users.noreply.github.com>
2023-03-31 08:35:09 -0500

    Brand dev (#1)
    * Add firebase package dependency
    
    * Add .env.development to ignore
    
    * Add config file for firebase connection

---
+++ src/firebase/config.ts
@@ -0,0 +1,23 @@
+import { initializeApp, getApps } from "firebase/app";
+
+interface FireBaseConfig {
+    apiKey: string,
+    authDomain: string,
+    projectId: string,
+    storageBucket: string,
+    messagingSenderId: string,
+    appId: string,
+}
+
+const fireBaseConfig: FireBaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+}
+
+const firebase_app = getApps().length === 0 ? initializeApp(fireBaseConfig) : getApps()[0];
+
+export default firebase_app;
