a8a2f5a4946ad2e7f63cebdbdf5170e3de41c916
Hitesh Ale <Hitech.tech101@gmail.com>
2023-04-05 11:11:25 -0400

    Added Dockerfile and docker-compose.yml for hot-reload dev enviroment

--- next.config.js
+++ next.config.js
@@ -3,6 +3,13 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  webpackDevMiddleware: config => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300,
+    }
+    return config
+  },
 }
 
 module.exports = nextConfig
